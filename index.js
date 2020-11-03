73775269b3300195cc09a96604672e3dfeee89cd
Mathis Barré <mathis.barre@live.fr>
2020-11-03 14:04:48 +0100

    fix bugs

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const app = require('express')()
 const http = require('http').createServer(app)
 const io = require('socket.io')(http)
-const _ = require('lodash')
+// const _ = require('lodash')
+const path = require('path')
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html')
+  res.sendFile(path.join(__dirname, '/index.html'))
 })
 
 io.on('connection', (socket) => {
@@ -12,20 +13,22 @@ io.on('connection', (socket) => {
   socket.broadcast.emit('user connected')
 
   socket.on('chat message', (payload) => {
-    console.log('Chat message : ' + payload.message);
-    socket.broadcast.emit('chat message', payload);
+    console.log('Chat message : ' + payload.message)
+    socket.broadcast.emit('chat message', payload)
   })
 
   socket.on('newUser', (pseudo) => {
-    socket.emit('newUser', pseudo);
+    socket.broadcast.emit('newUserServer', pseudo)
+    console.log(pseudo + " s'est connecté")
   })
 
   socket.on('removeUser', (pseudo) => {
-    socket.emit('removeUser', pseudo);
+    socket.broadcast.emit('removeUserServer', pseudo)
+    console.log(pseudo + " s'est déconnecté")
   })
 
   socket.on('disconnect', () => {
-    console.log('Un utilisateur s\'est déconnecté');
+    console.log('Un utilisateur s\'est déconnecté')
     socket.broadcast.emit('user disconnected')
   })
 })
